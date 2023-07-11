a1c7efe218939698dd05bcd71622e23bd03adce5
Sanjay S Gangwar <contact.gangwar@gmail.com>
2023-07-11 16:26:10 +0530

    Initial Commit

---
+++ mRoute/mRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  mRouteApp.swift
+//  mRoute
+//
+//  Created by Sanjay on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct mRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
